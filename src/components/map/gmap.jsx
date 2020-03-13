408ed1fcf2b8f51894668c6acc1a195616818691
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-12 18:25:02 -0700

    add serach box and remove map type controls

--- src/components/map/gmap.jsx
+++ src/components/map/gmap.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import MapContainer from './container.css';
+import SearchBox from './searchBox.css';
 
 const API_KEY = process.env.GMAP_API_KEY;
-const endpoint = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+const endpoint = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
 
 const GoogleMap = (props) => {
 
@@ -12,10 +13,13 @@ const GoogleMap = (props) => {
 
     const [state, setState] = React.useState({
         gmap: null,
+        searchBox: null,
+        markers: [],
         center: { lat: 30.5928, lng: 114.3055 },
         userLocation: null,
     });
     const mapRef = React.useRef();
+    const searchBoxRef = React.useRef();
 
     /*
      *  Functions
@@ -75,7 +79,12 @@ const GoogleMap = (props) => {
         script.onload = () => {
             const gmap = new window.google.maps.Map(
                 mapRef.current,
-                { center: state.center , zoom: 12 }
+                {
+                    center: state.center,
+                    zoom: 12,
+                    mapTypeId: 'roadmap',
+                    mapTypeControl: false,
+                }
             );
             // assign gmap instance to state
             setState(s => ({
@@ -87,8 +96,88 @@ const GoogleMap = (props) => {
         document.getElementsByTagName('head')[0].appendChild(script);
     }, []);
 
-    // get user location after map is initiated
-    React.useEffect(getUserLocation, [state.gmap]);
+    // initialize map elements after api load
+    React.useEffect(() => {
+        if ('google' in window && state.gmap) {
+            // insert search box
+            setState(s => ({
+                ...s,
+                searchBox: new window.google.maps.places.SearchBox(searchBoxRef.current)
+            }));
+        }
+    }, [state.gmap]);
+
+    // initialize search box properties
+    React.useEffect(() => {
+        if ('google' in window && state.gmap && state.searchBox) {
+            state.gmap.controls[window.google.maps.ControlPosition.TOP_LEFT].push(searchBoxRef.current);
+
+            // bias searchBox results toward current map position
+            state.gmap.addListener('bounds_changed', function() {
+                state.searchBox.setBounds(state.gmap.getBounds());
+            });
+
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            state.searchBox.addListener('places_changed', function() {
+                const places = state.searchBox.getPlaces();
+
+                if (places.length < 1) {
+                    return;
+                }
+
+                // clear old markers
+                state.markers.forEach(m => m.setMap(null));
+
+                // hydrate new markers
+                const markers = [];
+
+                // for each place, get icon, name, and location
+                const bounds = new window.google.maps.LatLngBounds();
+                places.forEach(place => {
+                    // check invalid data
+                    if (!place.geometry) {
+                        console.log("Returned place contains no geometry");
+                        return;
+                    }
+                    // define icon
+                    const icon = {
+                        url: place.icon,
+                        size: new window.google.maps.Size(71, 71),
+                        origin: new window.google.maps.Point(0, 0),
+                        anchor: new window.google.maps.Point(17, 34),
+                        scaledSize: new window.google.maps.Size(25, 25)
+                    };
+                    // create marker for place
+                    markers.push(new window.google.maps.Marker({
+                        map: state.gmap,
+                        icon,
+                        title: place.name,
+                        position: place.geometry.location,
+                    }));
+                    // sync viewport bounds
+                    if (place.geometry.viewport) {
+                        // only geocodes have viewport
+                        bounds.union(place.geometry.viewport);
+                    } else {
+                        bounds.extend(place.geometry.location);
+                    }
+                });
+
+                // update map bounds
+                state.gmap.fitBounds(bounds);
+                setState(s => ({
+                    ...s,
+                    markers,
+                }));
+            })
+
+        }
+
+        // finish by retrieving user location
+        getUserLocation();            
+
+    }, [state.searchBox])
 
     // update center upon state change
     React.useEffect(() => {
@@ -99,6 +188,7 @@ const GoogleMap = (props) => {
 
     return (
         <MapContainer ref={mapRef}>
+            <SearchBox ref={searchBoxRef} placeholder="Search a location" />
         </MapContainer>
     );
 }
