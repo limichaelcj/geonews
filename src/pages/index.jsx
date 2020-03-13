84be5d49a0daac576975e99c5883e3cb81cac65c
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-13 00:07:59 -0700

    refactor gmap to initializer and control components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,14 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { AppContainer } from './index.css';
 import ArticlePanel from '../components/article/panel';
-import GoogleMap from '../components/map/gmap';
+import GmapInitializer from '../components/map/initializer';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="COVID-19 Tracker" />
     <AppContainer>
       <ArticlePanel />
-      <GoogleMap />
+      <GmapInitializer />
     </AppContainer>
   </Layout>
 )
