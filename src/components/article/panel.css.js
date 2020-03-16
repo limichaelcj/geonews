1c0eec3e3b45eddfa912cb7c2f4ae3b55f8bba4b
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-16 16:18:38 -0700

    init newsapi fetch in articlepanel

--- src/components/article/panel.css.js
+++ src/components/article/panel.css.js
@@ -4,8 +4,8 @@ export default styled.ul`
     margin: 0;
     display: flex;
     flex: 0 0 240px;
-    min-width: 240px;
-    max-width: 240px;
+    min-width: ${props => props.theme.panel.width};
+    max-width: ${props => props.theme.panel.width};
     height: 100%;
     box-shadow: ${props => props.theme.shadow.sm};
     padding: 2rem;
--- src/components/article/panel.jsx
+++ src/components/article/panel.jsx
@@ -2,13 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Styled from './panel.css';
 
+const apiKey = process.env.NEWS_API_KEY;
+
 const ArticlePanel = ({articles}) => {
+    
+    const [state, setState] = React.useState({
+        loading: true,
+        articles: [],
+    });
+    
+    const fetchArticles = function(locations) {
+        // get date params        
+        const lastWeek = new Date();
+        lastWeek.setDate(new Date().getDate() - 7);
+        const dateString = `${lastWeek.getFullYear()}-${('0'+(lastWeek.getMonth()+1)).slice(-2)}-${('0'+lastWeek.getDate()).slice(-2)}`;
+        // form keyword/location query
+        const keywords = ['coronavirus', ...locations].join(' ').replace(' ', '%20');
+        // build api endpoint
+        const endpoint = `http://newsapi.org/v2/everything?q="${keywords}"&from=${dateString}&sortBy=publishedAt&apiKey=${apiKey}`;
+        console.log(endpoint);
+        // fetch api
+        fetch(endpoint).then(res => {
+            return res.json();
+        }).then(res => {
+            setState(s => ({
+                loading: false,
+                articles: res.articles,
+            }));
+        });
+    }
+
+    React.useEffect(() => {
+        console.log('fetching articles')
+        fetchArticles();
+        setState(s => ({
+            ...s,
+            loading: true,
+        }));
+    }, []);
+
     return (
         <Styled>
             <h4>Articles</h4>
-            {articles.length > 0 ? articles.map((a,i) => (
+            {state.articles.length > 0 ? state.articles.map((a,i) => (
                 <li key={i}>
-                    {i}
+                    {a.title}
                 </li>
             )) : (
                 <p>No articles</p>
