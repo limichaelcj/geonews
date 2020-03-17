efdab8ce8b165286e917122fad37c2459d202528
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-16 23:03:20 -0700

    add locale injection and filtering in article data

--- src/components/article/panel.jsx
+++ src/components/article/panel.jsx
@@ -17,9 +17,20 @@ const ArticlePanel = ({locales}) => {
      *  Functions
      */
 
+    // remove metadata in article.content string
     const formatContent = (content) => {
         if (!content) return '';
         return content.replace(/(<.*>|\[.*chars\]$)/g, '');
+    }
+
+    const getTopicLocale = function(article) {
+        if (!locales || locales.length < 1) return;
+
+        locales.forEach(lname => {
+            if (article.title.includes(lname) || article.content.includes(lname)) {
+                return article.locale = lname;
+            }
+        })
     }
     
     // Get articles from News API.
@@ -27,9 +38,9 @@ const ArticlePanel = ({locales}) => {
     // return top headlines without location filtering.
     const fetchArticles = function() {
         // get date params        
-        const lastWeek = new Date();
-        lastWeek.setDate(new Date().getDate() - 7);
-        const dateString = `${lastWeek.getFullYear()}-${('0'+(lastWeek.getMonth()+1)).slice(-2)}-${('0'+lastWeek.getDate()).slice(-2)}`;
+        const fromDate = new Date();
+        fromDate.setDate(new Date().getDate() - 14);
+        const dateString = `${fromDate.getFullYear()}-${('0'+(fromDate.getMonth()+1)).slice(-2)}-${('0'+fromDate.getDate()).slice(-2)}`;
         // form keyword/location query
         const topicQuery = `(coronavirus OR covid-19)`;
         const localeQuery = locales.length > 0 ? ` AND (${locales.join(' OR ')})` : '';
@@ -41,9 +52,16 @@ const ArticlePanel = ({locales}) => {
         fetch(endpoint).then(res => {
             return res.json();
         }).then(res => {
+
+            const articles = res.articles || [];
+        
+            articles.forEach(art => {
+                getTopicLocale(art);
+            })
+
             setState(s => ({
                 loading: false,
-                articles: res.articles || [],
+                articles,
             }));
         });
     }
@@ -69,9 +87,9 @@ const ArticlePanel = ({locales}) => {
 
     return (
       <Panel ref={panelRef}>
-        <h4>Articles{locales.length > 0 ? ' near ' + locales.join(', ') : ''}</h4>
+        <h4>News{locales.length > 0 ? " near " + locales.join(", ") : ""}</h4>
         {state.articles.length > 0 ? (
-          state.articles.map((a, i) => (
+          state.articles.filter(a => !!a.locale).map((a, i) => (
             <li key={i}>
               <a href={a.url} target="__blank" rel="noreferrer noopener">
                 <h5>
@@ -80,8 +98,15 @@ const ArticlePanel = ({locales}) => {
               </a>
               <h6>
                 <em>{a.author}</em>
+                {a.locale && (
+                    <>
+                        <span> &ndash; </span>
+                        <em>{a.locale}</em>
+                    </>
+                )}
               </h6>
               <p>{formatContent(a.content || a.description)}</p>
+              <em>{new Date(a.publishedAt).toDateString()}</em>
             </li>
           ))
         ) : (
