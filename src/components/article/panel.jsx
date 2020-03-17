b19dd093cc2935e1ec14372eec9f65ef145e9440
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-16 19:09:36 -0700

    connect article search to google map location

--- src/components/article/panel.jsx
+++ src/components/article/panel.jsx
@@ -24,10 +24,11 @@ const ArticlePanel = ({locales}) => {
         lastWeek.setDate(new Date().getDate() - 7);
         const dateString = `${lastWeek.getFullYear()}-${('0'+(lastWeek.getMonth()+1)).slice(-2)}-${('0'+lastWeek.getDate()).slice(-2)}`;
         // form keyword/location query
-        const keywords = ['coronavirus', ...locales].join(' ').replace(' ', '%20');
+        const topicQuery = `(coronavirus OR covid-19)`;
+        const localeQuery = locales.length > 0 ? ` AND (${locales.join(' OR ')})` : '';
+        const keywords = encodeURI(`${topicQuery}${localeQuery}`);
         // build api endpoint
-        const endpoint = `http://newsapi.org/v2/everything?q="${keywords}"&from=${dateString}&sortBy=publishedAt&apiKey=${apiKey}`;
-        console.log(endpoint);
+        const endpoint = `http://newsapi.org/v2/everything?q=${keywords}&from=${dateString}&sortBy=publishedAt&apiKey=${apiKey}&language=en`;
         // fetch api
         fetch(endpoint).then(res => {
             return res.json();
@@ -39,27 +40,40 @@ const ArticlePanel = ({locales}) => {
         });
     }
 
+    /*
+     *  Effects
+     */
+
+    // fetch articles on init and locales update
     React.useEffect(() => {
-        console.log('fetching articles')
         fetchArticles();
         setState(s => ({
             ...s,
             loading: true,
         }));
-    }, []);
+    }, [locales]);
+
+    /*
+     *  Render
+     */
 
     return (
-        <Styled>
-            <h4>Articles</h4>
-            {state.articles.length > 0 ? state.articles.map((a,i) => (
-                <li key={i}>
-                    {a.title}
-                </li>
-            )) : (
-                <p>No articles</p>
-            )}
-        </Styled>
-    );
+      <Styled>
+        <h4>Articles</h4>
+        {state.articles.length > 0 ? (
+          state.articles.map((a, i) => (
+            <li key={i}>
+              <h6>
+                <strong>{a.title}</strong>
+              </h6>
+              <p>{a.description}</p>
+            </li>
+          ))
+        ) : (
+          <p>No articles</p>
+        )}
+      </Styled>
+    )
 }
 
 ArticlePanel.propTypes = {
